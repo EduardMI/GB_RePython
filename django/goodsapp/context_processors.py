bcc7a1ce1a4eb50dac2cea4f4d7cdfe41cf95261
Makeev Eduard <mckena@mail.ru>
2022-06-01 11:26:04 +0300

    lesson_6_django

--- django/goodsapp/context_processors.py
+++ django/goodsapp/context_processors.py
@@ -1,5 +1,9 @@
+from django.contrib.sites.shortcuts import get_current_site
+
 from .models import Category
 
 
 def menu(request):
-    return {'category_list': Category.objects.all()}
+    # return {'category_list': Category.objects.all()}
+    return {'category_list': Category.on_site.all(),
+            'site': get_current_site(request.site)}
